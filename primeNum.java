093000603b4257d5cc18d53516cd29bf76df4d7f
Prasuna Dash <110610976+dprasuna@users.noreply.github.com>
2023-07-05 21:13:03 +0530

    Add files via upload

---
+++ primeNum.java
@@ -0,0 +1,32 @@
+// CODE BY PRASUNA DASH
+
+import java.util.Scanner;
+
+public class primeNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        boolean isPrime = true;
+        
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+        
+        scanner.close();
+    }
+}
