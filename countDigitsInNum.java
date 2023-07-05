307271ad7af4cfc103ded63663b60f479a214481
Prasuna Dash <110610976+dprasuna@users.noreply.github.com>
2023-07-05 21:14:45 +0530

    Add files via upload

---
+++ countDigitsInNum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class countDigitsInNum {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int dig = 0;
+        while (n != 0) {
+            n = n/10;
+            dig++;
+        }
+        System.out.println(dig);
+    }
+}
---
+++ digitsOfNum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class digitsOfNum {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int numberOfDigits = 0;
+        int temp = n;
+
+        while (temp!=0) {
+            temp = temp/10;
+            numberOfDigits++;
+        }
+
+        int div = (int)Math.pow(10, numberOfDigits - 1);
+        while (div != 0) {
+            int q = n/div;
+            System.out.println(q);
+
+            n = n % div;
+            div = div / 10;
+
+        }
+    }
+}
